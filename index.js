96ed8399a34059d2948ead3408ea1161053e7722
Chris Daniels <danielschris96@gmail.com>
2023-04-23 16:48:09 -0700

    added user input to data response

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const {Circle, Triangle, Square} = require("./lib/shapes.js");
 
 function init () {
     inquirer
@@ -9,6 +10,14 @@ function init () {
                 type: 'input',
                 message: 'What characters (no more than 3) would you like your logo to display?',
                 name: 'characters',
+                validate: function (input) {
+                    if (input.length < 4) {
+                        return true;
+                    }
+                    else {
+                        return 'Please enter 3 or less characters';
+                    }
+                }
             },
             {
                 type: 'input',
@@ -27,4 +36,23 @@ function init () {
                 name: 'shapeColor',
             },
         ])
-    }
+        .then((data) => {
+            let shape;
+            if (data.shape === "Circle") {
+              shape = new Circle(data.characters, data.textColor, data.shapeColor);
+            } else if (data.shape === "Triangle") {
+              shape = new Triangle(data.characters, data.textColor, data.shapeColor);
+            } else if (data.shape === "Square") {
+              shape = new Square(data.characters, data.textColor, data.shapeColor);
+            }
+        
+            const logo = generateSVG(shape);
+        
+            fs.writeFile("logo.svg", logo, (err) =>
+              err ? console.log(err) : console.log("Successfully generated logo.svg")
+            );
+          });
+};
+
+
+init()
